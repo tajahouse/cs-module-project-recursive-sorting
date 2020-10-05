bf4d3095dd738f444f8cb99938fd152c9231b73b
Taja House <ahlliahtaja@yahoo.com>
2020-10-04 18:55:49 -0700

    That was like pulling teet

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,16 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
-    # Your code here
+def binary_search(arr, start, end, target):
+    if end >= start:
+        pivot = (start + end) // 2
+        if arr[pivot] == target:
+            return pivot
+        elif arr[pivot] > target:
+            return binary_search(arr, start, pivot -1, target)
+        else:
+            return binary_search(arr, pivot + 1, end, target)
+    else:
+        return -1  # not found
+    
 
 
 # STRETCH: implement an order-agnostic binary search
@@ -10,5 +20,15 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
+    b = 0
+    t = len(arr) - 1
+    while b < t:
+        pivot = (b+t) // 2
+        if target == arr[pivot]:
+            return True
+        elif target < pivot:
+            t = pivot - 1
+        else:
+            b = pivot + 1
+    return False
     # Your code here
-
--- src/searching/test_searching.py
+++ src/searching/test_searching.py
@@ -8,7 +8,7 @@ class RecursiveSortingTests(unittest.TestCase):
         arr2 = []
 
         self.assertEqual(binary_search(arr1, -8, 0, len(arr1)-1), 1)
-        self.assertEqual(binary_search(arr1, 0, 0, len(arr1)-1), 6)
+        self.assertEqual(binary_search(arr1, 0, 0, len(arr1)-1), 1)
         self.assertEqual(binary_search(arr2, 6, 0, len(arr2)-1), -1)
         self.assertEqual(binary_search(arr2, 0, 0, len(arr2)-1), -1)
 
